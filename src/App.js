35a880cda1500bbd3d1bcc61e7148bdc85e697f0
Isaack Brian <isaackokello@gmail.com>
2020-08-04 15:05:36 +0200

    modifying App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button type="primary" size="large">Primary Button</Button>
       </header>
     </div>
   );
